ed277d378dff259becf0a30e10cc4c975a44fe72
Mac <Elay@iMac.local>
2015-08-11 09:12:13 +0800

    Initial Commit

---
+++ starRating/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  starRating
+//
+//  Created by Mac on 15/8/11.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ starRating/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  starRating
+//
+//  Created by Mac on 15/8/11.
+//  Copyright (c) 2015年 Mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
